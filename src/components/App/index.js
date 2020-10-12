1b96e2d7d1f31a6da9b57293c1e488e01d63898b
robmitch802 <60521912+robmitch802@users.noreply.github.com>
2020-10-12 16:29:51 -0400

    update with routes'

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navigation from './Navigation';
 
 const App = () => (
-    <div>
-        <h1>App</h1>
-    </div>
+    <Router>
+        <Navigation />
+    </Router>
 );
 
 export default App;
