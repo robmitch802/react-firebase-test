025f5bbbde2984bf02d0708054e922025f6a48ca
robmitch802 <60521912+robmitch802@users.noreply.github.com>
2020-10-12 17:28:20 -0400

    firebase mid-setup

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const SignUp = () => (
+import * as ROUTES from '../../constants/routes';
+
+const SignUpPage = () => (
     <div>
         <h1>Sign Up</h1>
+        <SignUpForm />
     </div>
 );
 
-export default SignUp;
+const INITIAL_STATE = {
+    username: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    error: null,
+}
+
+class SignupForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            ...INITIAL_STATE
+        };
+    };
+
+    onSubmit = event => {
+
+    };
+
+    onChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    render() {
+        const {
+            username,
+            email,
+            passwordOne,
+            passwordTwo,
+            error,
+        } = this.state;
+        
+        return (
+            <form onSubmit={this.onSubmit} >
+
+
+            </form>
+        )
+    }
+};
+
+const SignUpLink = () => (
+    <p>Don't have an account? <Link to={ROUTES.SIGN_UP} >Sign Up</Link></p>
+);
+
+export default SignUpPage;
+
+export { SignupForm, SignUpLink };
