643f784f4a06bd28a051f066650154d83c8ee567
robmitch802 <60521912+robmitch802@users.noreply.github.com>
2020-10-12 16:39:21 -0400

    nav setup

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import * as ROUTES from '../../constants/routes';
 
 const Navigation = () => (
     <div>
-        <h1>Navigation</h1>
+        <Container >
+            <Navbar fixed="top" bg="dark" variant="dark" expand="md" collapseOnSelect="true" >
+                <Navbar.Brand>
+                    <Link to="/" >
+                        <span><strong>RM</strong></span>
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto body-text" >
+                        <Nav.Link to={ROUTES.HOME} >Home</Nav.Link>
+                        <Nav.Link to={ROUTES.LANDING} >Landing</Nav.Link>
+                        <Nav.Link to={ROUTES.ACCOUNT} >Account</Nav.Link>
+                        <Nav.Link to={ROUTES.ADMIN} >Admin</Nav.Link>
+                        <Nav.Link to={ROUTES.SIGN_IN} >Sign In</Nav.Link>
+                    </Nav> 
+                </Navbar.Collapse>
+            </Navbar>
+        </Container>
     </div>
 );
 
