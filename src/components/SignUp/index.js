5269aa1bac3271899db49178c1b75c0dc372d916
robmitch802 <60521912+robmitch802@users.noreply.github.com>
2020-10-12 21:54:19 -0400

    signup form

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Form } from 'react-bootstrap';
 
 import * as ROUTES from '../../constants/routes';
 
@@ -43,13 +44,67 @@ class SignupForm extends Component {
             passwordTwo,
             error,
         } = this.state;
-        
+
+        const isInvalid =
+            passwordOne !== passwordTwo ||
+            passwordOne === '' ||
+            email === '' ||
+            username === '';
+
         return (
-            <form onSubmit={this.onSubmit} >
 
+            <Form onSubmit={this.onSubmit} >
+                <Form.Group controlId="username" >
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control 
+                        type="text" 
+                        name="username" 
+                        value={username} 
+                        onChange={this.onChange} 
+                        placeholder="Full Name" 
+                    />
+                </Form.Group>
+                <Form.Group controlId="formEmail">
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control 
+                        type="email" 
+                        name="email"
+                        value={email}
+                        onChange={this.onChange}
+                        placeholder="Enter email" 
+                    />
+                    <Form.Text className="text-muted" >
+                        We'll never share your email. 
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control 
+                        type="text"
+                        name="passwordOne"
+                        value={passwordOne}
+                        onChange={this.onChange}
+                        placeholder="Password"
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Password Verify</Form.Label>
+                    <Form.Control 
+                        type="text"
+                        name="passwordTwo"
+                        value={passwordTwo}
+                        onChange={this.onChange}
+                        placeholder="Password"
+                    />
+                </Form.Group>
+                <Button disabled={isInvalid} variant="primary" type="submit" >
+                    Sign Up
+                </Button>
+                
+                {error && <p>{error.message}</p>}
 
-            </form>
-        )
+            </Form>
+        );
     }
 };
 
